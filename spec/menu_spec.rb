3418f569c9ce1a8bbcae5722d0831936530e9cdf
NoelleDL <noelle.legrain@gmail.com>
2018-04-17 21:45:09 +0100

    removed vacuous test in menu_spec

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,12 +4,6 @@ describe Menu do
 
   subject(:menu) { described_class.new }
 
-  describe '#initialize' do
-    it 'initializes menu class with menu items' do
-      expect(menu.dishes).to be_a(Hash)
-    end
-  end
-
   describe '#display_menu' do
     it 'displays menu items and prices' do
       expect(menu.display_menu).to eq(menu.dishes)
